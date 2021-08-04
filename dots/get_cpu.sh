b69c1840ba20bc6ea854ddd21e9cf7ef28176950
Rosen Popov <rpopov@mm-sol.com>
2021-08-04 18:33:26 +0300

    tmux: Better cpu load display

--- dots/get_cpu.sh
+++ dots/get_cpu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -n $(vmstat|awk 'NR==3{printf " #[bg=#ffe6b3,fg=#14191f] C "$13+$14"%"}') \
-        $(free|awk 'NR==2{printf "#[fg=#14191F,bg=#87dfeb] R %.1f/%.1f",$3/1000000,$2/1000000}') \
+echo -n $(ps -aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {printf "#[bg=#ffe6b3,fg=#14191f] Cpu %.1f%",s/'$(nproc)'}') \
+        $(free|awk 'NR==2{printf "#[fg=#14191F,bg=#87dfeb] Ram %.1f/%.1f",$3/1000000,$2/1000000}') \
         $(df |awk 'NR==4{printf "#[fg=#14191F,bg=#d4bfff] %.1f Gb ",$4/1000000}')
