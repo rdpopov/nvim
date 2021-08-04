fc2824db1dfdb446eb8ae640a100c15051dd1e1b
Rosen Popov <rpopov@mm-sol.com>
2021-08-04 13:27:35 +0300

    tmux: Colors

--- dots/get_cpu.sh
+++ dots/get_cpu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -n $(vmstat|awk 'NR==3{printf " #[bg=#f02e6e,fg=#14191f] C "$13+$14"%"}') \
+echo -n $(vmstat|awk 'NR==3{printf " #[bg=#ffe6b3,fg=#14191f] C "$13+$14"%"}') \
         $(free|awk 'NR==2{printf "#[fg=#14191F,bg=#87dfeb] R %.1f/%.1f",$3/1000000,$2/1000000}') \
-        $(df |awk 'NR==4{printf "#[fg=#14191F,bg=#ffe6b3] %.1f Gb ",$4/1000000}')
+        $(df |awk 'NR==4{printf "#[fg=#14191F,bg=#d4bfff] %.1f Gb ",$4/1000000}')
