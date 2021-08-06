d2f62612b105439b27646a118d53e711adfafc2b
Rosen Popov <rpopov@mm-sol.com>
2021-08-06 10:41:03 +0300

    Everytthing is moved into

---
+++ dots/status.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo -n "#[fg=#14191f,bg=#f02e6e]"$(date +" "%d/%b) "#[fg=#14191F,bg=#62d196]"$(date +" "%H:%M) \
+        $(ps -aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {printf "#[bg=#ffe6b3,fg=#14191f] Cpu %.1f%",s/'$(nproc)'}') \
+        $(free|awk 'NR==2{printf "#[fg=#14191F,bg=#87dfeb] Ram %.1f/%.1f",$3/1000000,$2/1000000}') \
+        $(df |awk 'NR==4{printf "#[fg=#14191F,bg=#d4bfff] %.1f Gb ",$4/1000000}')
