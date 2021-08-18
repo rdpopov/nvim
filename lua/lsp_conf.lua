67091e910b95d161e2dafd52736f2ef6fc367ff6
Rosen Popov <rdonkow@gmail.com>
2021-08-18 14:08:19 +0300

    lsp:  added pylsp support

--- lua/lsp_conf.lua
+++ lua/lsp_conf.lua
@@ -1,12 +1,13 @@
 require'lspconfig'.nimls.setup{}
 require'lspconfig'.ccls.setup{}
-require'lspconfig'.jedi_language_server.setup{}
+require'lspconfig'.pylsp.setup{}
 require'lspconfig'.tsserver.setup{}
 require'lspconfig'.html.setup{}
 require'lspconfig'.gopls.setup{}
 require'lspconfig'.vimls.setup{}
 require'lspconfig'.rls.setup{}
 require'lspconfig'.hls.setup{}
+require'lspconfig'.racket_langserver.setup{}
 
 require "lsp_signature".setup({
     bind = true, 
