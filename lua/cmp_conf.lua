98d391a34175405a71fa393db8d36e0b940be315
Rosen Popov <rdonkow@gmail.com>
2021-09-10 21:17:42 +0300

    cmp:    moved away grom clangd to ccls

--- lua/cmp_conf.lua
+++ lua/cmp_conf.lua
@@ -33,7 +33,7 @@ cmp.setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 -- The following example advertise capabilities to `clangd`.
-require'lspconfig'.clangd.setup { capabilities = capabilities, }
+require'lspconfig'.ccls.setup { capabilities = capabilities, }
 require'lspconfig'.nimls.setup { capabilities = capabilities, }
 require'lspconfig'.ccls.setup { capabilities = capabilities, }
 require'lspconfig'.pylsp.setup { capabilities = capabilities, }
