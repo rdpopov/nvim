af72be95e1d9d4d143b07f273a91da8482e8f809
Rosen Popov <50305462+rdpopov@users.noreply.github.com>
2021-09-23 18:46:17 +0300

    setup: better setup script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ apt_install(){
 }
 
 setup_deps(){
-    apt_install fuse libfuse2 git python3-pip ack-grep 
-    apt_install nodejs npm yarn
+    apt_install fuse libfuse2 git python3-pip ack-grep curl
     apt_install ripgrep
 }
 
@@ -22,8 +21,11 @@ setup_nvim(){
 setup_tmux(){
     [ -e "$HOME/.tmux.conf" ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
     cp dots/tmux.conf $HOME/.tmux.conf
-    [ -d "$HOME/.tmux/plugins/tpm" ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm #tmux package manager
+    [ -d "$HOME/.tmux/plugins/tpm" ] || git clone https://www.github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm #tmux package manager
     [[ $(grep ~/.bashrc -e env\.sh -c) == "0" ]] && echo "source ~/.config/nvim/dots/env.sh" >> ~/.bashrc || echo "already in .bashrc"
+}
+setup_quoty(){
+    [ -d "$HOME/.config/quoty" ] && cd $HOME/.config/quoty/; git stash; git pull || cd $HOME/.config/;git clone https://www.github.com/rdpopov/quoty
 }
 
 setup_alac(){
@@ -31,25 +33,49 @@ setup_alac(){
     cp dots/alacritty.yml $HOME/.alacritty.yml
 }
 
-for arg in "$@";do
-  case "$arg" in
-    "--tmux" | "-t" )
-      setup_tmux
-    ;;
-    "--alac" | "-a" )
-      setup_alac
-    ;;
-    "--nvim" | "-n" )
-      setup_nvim
-    ;;
-    "--deps" | "-d" )
-      setup_deps
-    ;;
-    "--all" | "-A" )
-      setup_deps
-      setup_nvim
-      setup_tmux
-      setup_alac
-    ;;
-  esac
-done
+if [[ $# == "0" ]]; then 
+  echo -e "Usage:";
+  echo -e "   --alac,-a\t setup alacritty ";
+  echo -e "   --tmux,-t\t sets up tmux and which requires quoty(optionally)"
+  echo -e "   --quoty,-q\t sets up quoty"
+  echo -e "   --nvim,-n\t sets up neovim latest appimage"
+  echo -e "   --deps,-d\t sets up dependacies for neovim"
+  echo -e "   --all,-A\t sets up all of the above for neovim"
+  exit 0;
+else
+  for arg in "$@";do
+    case "$arg" in
+      "--quoty" | "-q" )
+        setup_quoty
+        ;;
+      "--tmux" | "-t" )
+        setup_tmux
+        ;;
+      "--alac" | "-a" )
+        setup_alac
+        ;;
+      "--nvim" | "-n" )
+        setup_nvim
+        ;;
+      "--deps" | "-d" )
+        setup_deps
+        ;;
+      "--all" | "-A" )
+        setup_deps
+        setup_nvim
+        setup_tmux
+        setup_alac
+        ;;
+      *)
+        echo -e "Usage:";
+        echo -e "   --alac,-a\t setup alacritty ";
+        echo -e "   --tmux,-t\t sets up tmux and which requires quoty(optionally)"
+        echo -e "   --quoty,-q\t sets up quoty"
+        echo -e "   --nvim,-n\t sets up neovim latest appimage"
+        echo -e "   --deps,-d\t sets up dependacies for neovim"
+        echo -e "   --all,-A\t sets up all of the above for neovim"
+        exit 0
+        ;;
+    esac
+  done
+fi
