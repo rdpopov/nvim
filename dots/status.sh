16146acc71e8fd24ef1571ef9f8b05fda85097bf
Rosen Popov <rdonkow@gmail.com>
2021-09-24 19:41:15 +0300

    tmux: statusline is now with a black background, so only one space is needed

--- dots/status.sh
+++ dots/status.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-battery="#[fg=#14191f,bg=#f02e6e]"$(upower -i /org/freedesktop/UPower/devices/battery_BAT1|awk 'NR>=19 && NR<=20 {if ($1 == "percentage:") print " ⭍" $2}')
-date="#[fg=#14191f,bg=#63f2f1]"$(date +" "%d/%b)
-time="#[fg=#14191F,bg=#62d196]"$(date +" "%H:%M)
-cpu=$(ps -aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {printf "#[bg=#ffe6b3,fg=#14191f] 📻%.1f%",s/'$(nproc)'}')
-ram=$(free|awk 'NR==2{printf "#[fg=#14191F,bg=#87dfeb] ɤ%.1f%",($3/$2)*100}')
-storage=$(df / |awk 'NR==2{printf "#[fg=#14191F,bg=#d4bfff] 💿%.1f",$4/1000000}')
-echo -n $battery $time $date $cpu $ram $storage
+battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1|awk 'NR>=19 && NR<=20 {if ($1 == "percentage:") print "#[bg=#1e1e1e,fg=#f02e6e] ⭍" $2}')
+date="#[bg=#1e1e1e,fg=#63f2f1]"$(date +" "%d/%b)
+time="#[bg=#1e1e1e,fg=#62d196]"$(date +" "%H:%M)
+cpu=$(ps -aux | awk '{print $3}' | tail -n+2 | awk '{s+=$1} END {printf "#[fg=#ffe6b3,bg=#1e1e1e] 📻%.1f%",s/'$(nproc)'}')
+ram=$(free|awk 'NR==2{printf "#[bg=#1e1e1e,fg=#87dfeb] ɤ%.1f%",($3/$2)*100}')
+storage=$(df / |awk 'NR==2{printf "#[bg=#1e1e1e,fg=#d4bfff] 💿%.1f",$4/1000000}')
+echo -n $battery$time$date$cpu$ram$storage
