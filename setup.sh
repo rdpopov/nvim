5d2351048c090cc916fba9463008b9da859ec3f4
Rosen Popov <50305462+rdpopov@users.noreply.github.com>
2023-05-06 23:55:29 +0000

    Fixed bug with setup file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Installs latest neovim appimage will clobber existing one
-wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
-chmod +x nvim && sudo chown root:root nvim && sudo mv nvim /usr/local/bin
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document ~/nvim
+chmod +x ~/nvim && sudo chown root:root ~/nvim && sudo mv ~/nvim /usr/local/bin
